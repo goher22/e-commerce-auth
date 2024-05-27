81026db993848a3a7f06553b9db498d917c5d4b9
Carlos <gohercarlos@gmail.com>
2024-05-26 21:26:48 -0500

    Init Proyect Auth

---
+++ src/main/java/com/code/whiskers/mic_e_commerce_auth/MicECommerceAuthApplication.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicECommerceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicECommerceAuthApplication.class, args);
+	}
+
+}
